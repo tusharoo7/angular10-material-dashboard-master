f31e6c03c3ed5d0373e2b5076cc5c1254e67550a
tusharoo7 <tusharrouth001@gmail.com>
2020-07-27 19:25:10 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,76 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { routing } from './app.routes';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FigurecardComponent } from './shared/figurecard/figurecard.component';
+import { ImagecardComponent } from './shared/imagecard/imagecard.component';
+import { TableComponent } from './dashboard/table/table.component';
+import { NotificationComponent } from './dashboard/notification/notification.component';
+import { MsgIconBtnComponent } from './shared/msgiconbtn/msgiconbtn.component';
+import { SweetAlertComponent } from './dashboard/sweetalert/sweetalert.component';
+import { LoginComponent } from './page/login/login.component';
+import { RootComponent } from './dashboard/root/root.component';
+import { RegisterComponent } from './page/register/register.component';
+import { LockComponent } from './page/lock/lock.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
+import { PriceTableComponent } from './dashboard/component/pricetable/pricetable.component';
+import { PanelsComponent } from './dashboard/component/panels/panels.component';
+
+import { SettingsService } from './services/settings.service';
+import { WizardComponent } from './dashboard/component/wizard/wizard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    ProfileComponent,
+    NavbarComponent,
+    FigurecardComponent,
+    ImagecardComponent,
+    TableComponent,
+    NotificationComponent,
+    MsgIconBtnComponent,
+    SweetAlertComponent,
+    LoginComponent,
+    RootComponent,
+    RegisterComponent,
+    LockComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent,
+    PriceTableComponent,
+    PanelsComponent,
+    WizardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatInputModule,
+    MatMenuModule,
+    MatCheckboxModule
+  ],
+  providers: [SettingsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
